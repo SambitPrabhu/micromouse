7b40ad91d0ffac9132ed1ca463826b6b1a3bb5e8
shreyassspp <shreyas_pp@ee.iitr.ac.in>
2021-12-07 12:03:33 +0530

    Checks for accesibility and updates wall configuration.

--- initial.py
+++ initial.py
@@ -6,7 +6,7 @@ from numpy import ma
 class robot:
                 
     def __init__(self,x_initial,y_initial,orient_inital):
-        self.maze=np.full((16,16),0) #Used for storing wall configuration.
+        self.self.maze=np.full((16,16),0) #Used for storing wall configuration.
 
         self.flood = np.full((16,16),255) #Used for storing the flood array and the costs which shall be used for traversal.
         
@@ -23,7 +23,7 @@ class robot:
         return False
 
 
-    def flood_initial(self):  # For generating the initial flooded maze assuming no walls are present.
+    def flood_initial(self):  # For generating the initial flooded self.maze assuming no walls are present.
 
    
         self.flood[7][7] =0 #Destination Cells are given weightage as zero.
@@ -40,11 +40,12 @@ class robot:
         
 
     def isEnd(self): #Checks if the bot has reached the destination.
-        if(self.maze[self.y][self.x]==0):
+        if(self.self.maze[self.y][self.x]==0):
             return True
         else:
             return False 
         #Need to add the functionality for storing the path in memory.
+        #If this returns true then we have to start the fast run.
 
     def giveSurroundings(self):
         #Returns the four neighbouring cells for a particular cell.
@@ -56,6 +57,104 @@ class robot:
         y3,x3 = self.y,self.x+1 #Cell at absolute west.
 
         return ((y0,x0),(y1,x1),(y2,x2),(y3,x3))
+    
+    def updateWalls(self,L,R,F):
+        #Gives the wall the configuration based on the presence of walls on left, right or front of the robot.
+        if(L and R and F):
+            if (self.orient==0): 
+                self.self.maze[self.y][self.x]= 13
+            elif (self.orient==1): 
+                self.maze[self.y][self.x]= 12
+            elif (self.orient==2): 
+                self.maze[self.y][self.x]= 11
+            elif (self.orient==3): 
+                self.maze[self.y][self.x]= 14
+
+        elif (L and R and not F):
+            if (self.orient==0 or self.orient== 2): 
+                self.maze[self.y][self.x]= 9
+            elif (self.orient==1 or self.orient==3): 
+                self.maze[self.y][self.x]= 10
+
+        elif (L and F and not R):
+            if (self.orient==0): 
+                self.maze[self.y][self.x]= 8
+            elif (self.orient==1): 
+                self.maze[self.y][self.x]= 7
+            elif (self.orient==2): 
+                self.maze[self.y][self.x]= 6
+            elif (self.orient==3): 
+                self.maze[self.y][self.x]= 5
+
+        elif (R and F and not L):
+            if (self.orient==0): 
+                self.maze[self.y][self.x]= 7
+            elif (self.orient==1): 
+                self.maze[self.y][self.x]= 6
+            elif (self.orient==2): 
+                self.maze[self.y][self.x]= 5
+            elif (self.orient==3): 
+                self.maze[self.y][self.x]= 8
+
+        elif(F):
+            if (self.orient==0): 
+                self.maze[self.y][self.x]= 2
+            elif (self.orient==1): 
+                self.maze[self.y][self.x]= 3
+            elif (self.orient==2): 
+                self.maze[self.y][self.x]= 4
+            elif (self.orient==3): 
+                self.maze[self.y][self.x]= 1
+
+        elif(L):
+            if (self.orient==0): 
+                self.maze[self.y][self.x]= 1
+            elif (self.orient==1): 
+                self.maze[self.y][self.x]= 2
+            elif (self.orient==2): 
+                self.maze[self.y][self.x]= 3
+            elif (self.orient==3): 
+                self.maze[self.y][self.x]= 4
+
+        elif(R):
+            if (self.orient==0): 
+                self.maze[self.y][self.x]= 3
+            elif (self.orient==1): 
+                self.maze[self.y][self.x]= 4
+            elif (self.orient==2): 
+                self.maze[self.y][self.x]= 1
+            elif (self.orient==3): 
+                self.maze[self.y][self.x]= 2
+
+    def isAccessible(self,x1,y1):
+    #Returns True if mouse can move to x1,y1 from x,y (it's current position).
+        if (self.x==x1):
+            if(self.y>y1):
+                if(self.self.maze[self.y][self.x]==4 or self.maze[self.y][self.x]==5 or self.maze[self.y][self.x]==6 or 
+                self.maze[self.y][self.x]==10 or self.maze[self.y][self.x]==11 or self.maze[self.y][self.x]==12 or self.maze[self.y][self.x]==14 ):
+                    return (False)
+                else:
+                    return(True)
+            else:
+                if(self.maze[self.y][self.x]==2 or self.maze[self.y][self.x]==7 or self.maze[self.y][self.x]==8 or self.maze[self.y][self.x]==10 or 
+                self.maze[self.y][self.x]==12 or self.maze[self.y][self.x]==13 or self.maze[self.y][self.x]==14 ):
+                    return (False)
+                else:
+                    return(True)
+            
+        elif (self.y==y1):
+            if(self.x>x1):
+                if(self.maze[self.y][self.x]==1 or self.maze[self.y][self.x]==5 or self.maze[self.y][self.x]==8 or 
+                self.maze[self.y][self.x]==9 or self.maze[self.y][self.x]==11 or self.maze[self.y][self.x]==13 or self.maze[self.y][self.x]==14 ):
+                    return (False)
+                else:
+                    return (True)
+            else:
+                if(self.maze[self.y][self.x]==3 or self.maze[self.y][self.x]==6 or self.maze[self.y][self.x]==7 or 
+                self.maze[self.y][self.x]==9 or self.maze[self.y][self.x]==11 or self.maze[self.y][self.x]==12 or self.maze[self.y][self.x]==13 ):
+                    return (False)
+                else:
+                    return (True)
 
 
 
